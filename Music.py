d9056e3d19183d8f230ce1aae2153c537fc4a9c4
[jordankravitz] <[jkravitz@usesi.com]>
2021-06-03 10:54:33 -0400

    dynamic working directory selection is working locally. Need to test on Jenkins

--- Music.py
+++ Music.py
@@ -9,18 +9,21 @@ import keyboard
 un = 'Automation'
 pw = 'vgYf9UeX7n23'
 
+#Gets current working directory
+cwd = os.getcwd()
+#sets the directory with the MOH files to the correct working directory + files (folder)
+dir_path = cwd + '''\\files'''
 
 #gets a list of all the files
-files = os.listdir('C:\\Users\\j.kravitz\\Python\\CUCM\\MOH\\files\\') 
-directory = '''C:\\Users\\j.kravitz\\Python\\CUCM\\MOH\\files\\'''
+files = os.listdir(dir_path) 
 
 
 for file in files:
     if file.startswith('X'):
-        os.rename(directory + file, directory + "Landover.wav")
+        os.rename(dir_path + '''\\'''+ file, dir_path + '''\\''' + "Landover.wav")
 
 
-newfiles = os.listdir("C:\\Users\\j.kravitz\\Python\\CUCM\\MOH\\files") 
+newfiles = os.listdir(dir_path) 
 
 server_ips = ['172.16.1.15', '172.16.1.10', '10.2.121.15']
 
@@ -28,7 +31,7 @@ server_ips = ['172.16.1.15', '172.16.1.10', '10.2.121.15']
 for file in newfiles:
     for server in server_ips:
         #print("filename "+ file + " ip: "+ server)
-        path = directory + file
+        file_path = dir_path + '''\\''' + file
         r.init()
         moh_url = 'https://' + server +'/ccmadmin/mohAudioFileUpload.do?type=mohAudioManagement'
         r.url(moh_url)
@@ -46,8 +49,7 @@ for file in newfiles:
 
         r.click ('FILE')
         r.wait(4)
-        keyboard.write(path)
-        r.wait(5)
+        keyboard.write(file_path)
         keyboard.press_and_release('tab, tab, enter')
         r.click('Upload File')
         r.wait(6)
