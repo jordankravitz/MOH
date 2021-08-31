a8dff7df6ef81a476db9b65b7b9d590d1c74dc80
[jordankravitz] <[jkravitz@usesi.com]>
2021-08-31 12:57:17 -0400

    Changed WWE to use the same zip function to unzip and delete the zip inside the folder

--- WWEupload.py
+++ WWEupload.py
@@ -3,6 +3,7 @@ import rpa as r
 import sys
 import keyboard
 from keyboard import press
+import unzip
 #import logging
 
 
@@ -23,6 +24,7 @@ pw = 'vgYf9UeX7n23'
 
 #hard coded dir_path for Jenkins server
 dir_path = "//automate/MOH/files"
+unzip.Zipcleanup(dir_path)
 
 #gets a list of all the files
 files = os.listdir(dir_path)
